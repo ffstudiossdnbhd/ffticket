a267257b920703cd8dbb411766a823cd7c9d32cf
Muazzam <muazzamhazmiii@gmail.com>
2026-07-31 15:09:47 +0800

    sql

--- backend/database/2026_07_31_add_category_to_faqs.sql
+++ backend/database/2026_07_31_add_category_to_faqs.sql
@@ -2,3 +2,4 @@ ALTER TABLE faqs
   ADD COLUMN category_id BIGINT UNSIGNED NULL AFTER description,
   ADD KEY idx_faqs_category_id (category_id),
   ADD CONSTRAINT fk_faqs_category FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL;
+  
